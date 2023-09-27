422dc302103e3a2b82f934059792676f14b7d899
QuinntonCarter <qdevsz@gmail.com>
2023-09-26 21:12:42 -0600

    Start orbit, need to account for earth radius

--- src/components/Moon.jsx
+++ src/components/Moon.jsx
@@ -4,22 +4,18 @@ import { useFrame } from "@react-three/fiber";
 export default function Moon({ earthRadius }) {
   const moonRef = useRef(null);
   useFrame((state, delta) => {
-    moonRef.current.rotation.y += delta * 0.01;
-    moonRef.current.rotation.z += delta * 0.01;
-    const x = Math.sin(delta);
-    const y = Math.cos(delta);
-    moonRef.current.position.y += Math.sin(x) * 0.05;
-    moonRef.current.position.x += Math.cos(y) * 0.05;
-
-    // moonRef.current.position.x += Math.cos(delta) * 0.05;
-    // moonRef.current.position.z += Math.cos(delta) * 0.05;
-
-    console.log(moonRef.current);
+    const time = state.clock.getElapsedTime() * 0.5;
+    // let moonPosY = moonRef.current.position.y + earthRadius;
+    // let moonPosX = moonRef.current.position.x + earthRadius;
+    moonRef.current.position.y += Math.cos(time) * 0.05;
+    moonRef.current.position.x += Math.sin(time) * 0.05;
+    // moonPosY += Math.cos(time) * 0.05;
+    // moonPosX += Math.sin(time) * 0.05;
   });
 
   return (
     <>
-      <mesh ref={moonRef} scale={[1, 1, 1]} position={[3, 6, 0]}>
+      <mesh ref={moonRef} scale={[1, 1, 1]} position={[-1, 0, 0]}>
         <sphereGeometry />
         <meshStandardMaterial color="lightgray" />
       </mesh>
