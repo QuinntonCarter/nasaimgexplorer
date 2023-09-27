a85d569f3604e68ac03d8944682b4d22da3aa94d
QuinntonCarter <qdevsz@gmail.com>
2023-09-26 22:11:47 -0600

    Orbit issue

--- src/components/Moon.jsx
+++ src/components/Moon.jsx
@@ -1,21 +1,23 @@
 import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
+import * as THREE from "three";
 
 export default function Moon({ earthRadius }) {
   const moonRef = useRef(null);
+  let testMoonVec = new THREE.Vector3(earthRadius);
   useFrame((state, delta) => {
     const time = state.clock.getElapsedTime() * 0.5;
-    // let moonPosY = moonRef.current.position.y + earthRadius;
-    // let moonPosX = moonRef.current.position.x + earthRadius;
-    moonRef.current.position.y += Math.cos(time) * 0.05;
-    moonRef.current.position.x += Math.sin(time) * 0.05;
-    // moonPosY += Math.cos(time) * 0.05;
-    // moonPosX += Math.sin(time) * 0.05;
+    moonRef.current.position.y +=
+      Math.cos(time + testMoonVec.x * 0.5 * -1) * 0.05;
+    moonRef.current.position.x +=
+      Math.sin(time + testMoonVec.x * 0.5 * -1) * 0.05;
+    moonRef.current.position.z +=
+      Math.sin(time + testMoonVec.x * 0.5 * -1) * 0.05;
   });
 
   return (
     <>
-      <mesh ref={moonRef} scale={[1, 1, 1]} position={[-1, 0, 0]}>
+      <mesh ref={moonRef} scale={[1, 1, 1]} position={[0, 0, 0]}>
         <sphereGeometry />
         <meshStandardMaterial color="lightgray" />
       </mesh>
